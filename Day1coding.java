e3b93dcc8d482190b64f8d729131cb3fa8b3a89c
Seramellia09 <seeramel09@gmail.com>
2026-09-02 22:28:00 +0800

    Create Day1coding.java

---
+++ Day1coding.java
@@ -0,0 +1,14 @@
+package mahasiswa;
+
+import java.util.Scanner;
+
+public class Coding{
+public static void main(String[] args) {
+  // komentar satu baris 
+  /* komentar lebih dari satu baris */
+  /** komentar dokumentas variabel */
+
+  System.out.println("selamat malam!");
+    
+}
+}
