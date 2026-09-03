dc77cbb2b3dd4fef28e537446869eb35dd808cc6
Seramellia09 <seeramel09@gmail.com>
2026-09-03 08:01:23 +0800

    Create Day2coding.java

---
+++ Day2coding.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Membuat biodata diri menggunakan print, println dan printf
+
+        String nama = "sera putri amellia";
+        String status = "mahasiswa";
+        int umur = 18;
+        double tinggi = 155.5;
+
+        // 1. menggunakan prtintln untuk membuat baris baru 
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("nama : sera putri amelllia");
+        System.out.println("status : mahasiswa");
+
+        // 2. menggunakan print untuk tidak memunculkan baris baru
+        System.out.print("umur : 18");
+
+        // 3. menggunakn printf untuk mencetak format khusus 
+        System.out.printf(" tinggi : 155.5");
+
+
+    }
+}
