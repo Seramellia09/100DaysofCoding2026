60b1dcc00556b17d4c6808b05b83c3ece27e9d33
Seramellia09 <seeramel09@gmail.com>
2026-09-04 07:11:23 +0800

    Create Day3coding.java

---
+++ Day3coding.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        // (\n) digunakan untuk memindahkan teks ke baris berikutnya
+        System.out.println("Hallo!\nPerkenalkan");
+
+        // (\t) digunakan untuk memberikan spasi selebar tab 
+        System.out.println("Nama:\tSera");
+
+        // (\") digunakan untuk menambahkan tanda kutip 
+        System.out.println("Dia berkata, \"hay!\"");
+
+        // (\\) digunakan untuk backlash
+        System.out.println("C:\\Folder\\File.txt");
+
+    }
+}
