4aefd29ecf61ccde7587bea05c7e7d4519bd2574
Seramellia09 <seeramel09@gmail.com>
2026-09-05 13:48:52 +0800

    Create Day5coding.java

---
+++ Day5coding.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+       /* double digunakan untuk menyimpan angka pecahan ukuran 64-bit 
+       dan memiliki kapasitas lebih besar dan lebih teliti */
+       /* float digunakan untuk menyimpan angka pecahan ukuran 32-bit dan 
+       pada float harus ditambahkan (f) pada akhir angka yang ditulis  */
+
+       double berat = 85.7654321;
+       float suhu = -45.7f;
+
+       System.out.println("berat: " + 85.7654321);
+       System.out.println("suhu: " + -45.7);
+       
+
+    }
+}
