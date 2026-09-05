77038811bdaf60cfa99172fd0058f6e7d83c2955
Seramellia09 <seeramel09@gmail.com>
2026-09-05 13:49:48 +0800

    Create Day6coding.java

---
+++ Day6coding.java
@@ -0,0 +1,22 @@
+public class App {
+     public static void main(String[] args) {
+
+        /* char digunakan untuk memunculkan karakter huruf, angka ataupun simbol. */
+        /* boolean digunakan untuk menyimpan dua nilai mutlak (true) dan (false). */
+
+        char huruf = 'A';
+        char angka = '9';
+        char simbol = '$';
+        boolean lulus = true;
+        boolean tidak_lulus = false;
+
+        System.out.println("huruf: " + 'A');
+        System.out.println("angka: " + 9);
+        System.out.println("simbol: " + '$');
+        System.out.println("lulus: " + true);
+        System.out.println("tidak_lulus: " + false);
+
+
+
+     }
+}
