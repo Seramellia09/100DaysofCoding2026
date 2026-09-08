e56528580d31d5b663a294c58be4c20fbd384611
Seramellia09 <seeramel09@gmail.com>
2026-09-08 18:31:55 +0800

    Create Day7coding.java

---
+++ Day7coding.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+// String adalah tipe data yang digunakan untuk menampilakan sebuah teks.
+
+String Nama = "Sera Putri Amellia";
+System.out.println("Nama: " + Nama);
+    
+    }
+}
