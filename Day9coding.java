264b684ee3c363402b0427ee633506d6485067d0
Seramellia09 <seeramel09@gmail.com>
2026-09-10 14:41:08 +0800

    Create Day9coding.java

---
+++ Day9coding.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+    final String NAMA = "sera putri amellia";
+    final int UKT = 1000000;
+    final double IPK = 4.00;
+    final boolean LULUS = true;
+
+    System.out.println("NAMA:" + NAMA);
+    System.out.println("UKT: " + UKT);
+    System.out.println("IPK: " + IPK);
+    System.out.println("LULUS: " + LULUS);
+
+    }
