a0aba4f11e0702db2e2a7e7892db991a66b36b34
Seramellia09 <seeramel09@gmail.com>
2026-09-14 19:07:39 +0800

    Create Day13coding.java

---
+++ Day13coding.java
@@ -0,0 +1,11 @@
+public class App {
+
+   public static void main(String[] args) throws Exception {
+
+     double angka1 = 3.55;
+     double angka2 = 6.99;
+
+System.out.printf("%.2f\n%.2f\n", angka1, angka2);
+
+   }
+}
