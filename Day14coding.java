aa9acc50776c1cb186efef4ac10a7477952a2a05
Seramellia09 <seeramel09@gmail.com>
2026-09-15 16:30:43 +0800

    Create Day14coding.java

---
+++ Day14coding.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+    int angka1 = 10;
+    int angka2 = 28;
+    int hasil_penjumlahan = angka1 + angka2;
+    int hasil_pengurangan = angka1 - angka2;
+
+    System.out.println("hasil_penjumlahan: " + hasil_penjumlahan);
+    System.out.println("hasil_pengurangan: " + hasil_pengurangan);
+
+    }
+}
