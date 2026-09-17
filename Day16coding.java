ce8f99ca129a820336cd8b76fc510c684dbbd0fa
Seramellia09 <seeramel09@gmail.com>
2026-09-17 18:48:52 +0800

    Create Day16coding.java

---
+++ Day16coding.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        
+    // deklarasi variabel
+    int angka1 = 77;
+    int angka2 = 5;
+
+    // perhitungan
+    int Hasil = angka1 % angka2;
+
+    // output
+    System.out.println("Modulus dari 77 % 5 adalah = " + Hasil);
+
+     }
+}
