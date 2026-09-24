02f2966d1cd6af6d036a4de80917bc7a459a2a54
Seramellia09 <seeramel09@gmail.com>
2026-09-24 16:46:20 +0800

    Create Day23coding.java

---
+++ Day23coding.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+    
+// menghitung luas persegi (rumus: sisi*sisi)
+    int sisi = 15;
+    int hasil = sisi*sisi;
+
+    System.out.println("hasil perhitungan persegi = " + hasil);
+
+    
+
+    }
+}
